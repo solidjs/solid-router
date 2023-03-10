7d549f67690b909cdfe730be6cd3f212a612f04e
gabrielelpidio <gabrieldeandradeleal@gmail.com>
2023-03-10 02:23:44 -0400

    Add memoryIntegration

--- src/integration.ts
+++ src/integration.ts
@@ -32,6 +32,49 @@ function scrollToHash(hash: string, fallbackTop?: boolean) {
   }
 }
 
+export function createMemoryHistory() {
+  const entries = ["/"];
+  let index = 0;
+  const listeners: ((value: string) => void)[] = [];
+
+  const go = (n: number) => {
+    // https://github.com/remix-run/react-router/blob/682810ca929d0e3c64a76f8d6e465196b7a2ac58/packages/router/history.ts#L245
+    index = Math.max(0, Math.min(index + n, entries.length - 1));
+
+    const value = entries[index];
+    listeners.forEach(listener => listener(value));
+  };
+
+  return {
+    get: () => entries[index],
+    set: ({ value, scroll, replace }: LocationChange) => {
+      if (replace) {
+        entries[index] = value;
+      } else {
+        entries.splice(index + 1, entries.length - index, value);
+        index++;
+      }
+      if (scroll) {
+        scrollToHash(value.split("#")[1] || "", true);
+      }
+    },
+    back: () => {
+      go(-1);
+    },
+    forward: () => {
+      go(1);
+    },
+    go,
+    listen: (listener: (value: string) => void) => {
+      listeners.push(listener);
+      return () => {
+        const index = listeners.indexOf(listener);
+        listeners.splice(index, 1);
+      };
+    }
+  };
+}
+
 export function createIntegration(
   get: () => string | LocationChange,
   set: (next: LocationChange) => void,
@@ -136,4 +179,11 @@ export function hashIntegration() {
       }
     }
   );
+}
+
+export function memoryIntegration() {
+  const memoryHistory = createMemoryHistory();
+  return createIntegration(memoryHistory.get, memoryHistory.set, memoryHistory.listen, {
+    go: memoryHistory.go
+  });
 }
