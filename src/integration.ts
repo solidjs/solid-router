40aedb2ac40abef1c9d95f0c23700353da9b8e2d
devinrhode2 <devinrhode2@gmail.com>
2023-08-22 21:34:41 -0500

    Edit comments in hashIntegration's `parsePath` util

--- src/integration.ts
+++ src/integration.ts
@@ -178,15 +178,15 @@ export function hashIntegration() {
       go: delta => window.history.go(delta),
       renderPath: path => `#${path}`,
       parsePath: str => {
+        // Get everything after the `#` (by dropping everything before the `#`)
         const to = str.replace(/^.*?#/, "");
-        // Hash-only hrefs like `#foo` from plain anchors will come in as `/#foo` whereas a link to
-        // `/foo` will be `/#/foo`. Check if the to starts with a `/` and if not append it as a hash
-        // to the current path so we can handle these in-page anchors correctly.
         if (!to.startsWith("/")) {
+          // We got an in-page heading link.
+          // Append it to the current path to maintain correct browser behavior.
           const [, path = "/"] = window.location.hash.split("#", 2);
           return `${path}#${to}`;
         }
-        return to;
+        return to; // Normal Solidjs <A> link
       }
     }
   );
