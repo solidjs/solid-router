d92290eddce9f355d7ce9236c74b33da557cebfe
Marvin <33938500+marvin-j97@users.noreply.github.com>
2024-11-08 18:40:14 +0100

    doc: useSearchParams jsdoc

--- src/routing.ts
+++ src/routing.ts
@@ -193,6 +193,33 @@ export const useCurrentMatches = () => useRouter().matches;
  */
 export const useParams = <T extends Params>() => useRouter().params as T;
 
+/**
+ * Retrieves a tuple containing a reactive object to read the current location's query parameters and a method to update them.
+ * The object is a proxy so you must access properties to subscribe to reactive updates.
+ * **Note** that values will be strings and property names will retain their casing.
+ * 
+ * The setter method accepts an object whose entries will be merged into the current query string.
+ * Values `''`, `undefined` and `null` will remove the key from the resulting query string.
+ * Updates will behave just like a navigation and the setter accepts the same optional second parameter as `navigate` and auto-scrolling is disabled by default.
+ * 
+ * @examples
+ * ```js
+ * const [searchParams, setSearchParams] = useSearchParams();
+ * 
+ * return (
+ *   <div>
+ *     <span>Page: {searchParams.page}</span>
+ *     <button
+ *       onClick={() =>
+ *         setSearchParams({ page: (parseInt(searchParams.page) || 0) + 1 })
+ *       }
+ *     >
+ *       Next Page
+ *     </button>
+ *   </div>
+ * );
+ * ```
+ */
 export const useSearchParams = <T extends SearchParams>(): [
   Partial<T>,
   (params: SetSearchParams, options?: Partial<NavigateOptions>) => void
